ad72d02c4ab922db2778e4ade3f6c0ed2a91d734
Lee <capn.lee@gmail.com>
2019-04-08 11:56:13 +0100

    commiting a fake dbConfig

---
+++ dbConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+	client: 'mysql',
+	debug: false,
+	connection: {
+		host : '127.0.0.1',
+		user : 'notarealusername',
+		password : 'notarealpassword',
+		database : 'notarealdb',  
+    	charset: "utf8mb4"
+	}
+}
